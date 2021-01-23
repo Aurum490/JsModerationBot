c6d5f991a98e90ddcc050b0a1442c774c3cf7912
Thenuka Herath <0033737@jwacs.wa.edu.au>
2021-01-23 13:57:26 +0800

    WOKCommands

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js')
+const WOKCommands = require('wokcommands')
 const client = new Discord.Client
 // const config = require('./config.json') 
 
 client.on('ready', () => {
-    console.log('The client is ready!')
+    console.log('READY!')
+
+    new WOKCommands(client)
 })
 
 client.login(process.env.TOKEN)
