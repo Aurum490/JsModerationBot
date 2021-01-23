539b7fb62674036c801d34df321e97ec3ed0db6a
Thenuka Herath <0033737@jwacs.wa.edu.au>
2021-01-23 17:20:46 +0800

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
 	bot_age,
 	bot_info,
 } = require('./config.json');
-client.on('ready', () => {
+client.once('ready', () => {
 	console.log(prefix);
 	console.log(bot_age);
 	console.log(bot_info.name);
@@ -19,6 +19,17 @@ client.on('ready', () => {
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split('/ +/');
+	const command = args.shift().toLowerCase();
+
+	if (command === 'args') {
+		if (!args.length) {
+			return message.channel.send(`You have not input any arguments, ${message.author}!`);
+		}
+
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	}
 	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong!');
 	} else if (message.content === `${prefix}Hi`) {
@@ -27,5 +38,8 @@ client.on('message', message => {
 		message.channel.send(message.guild.name);
 	} else if (message.content === `${prefix}members`) {
 		message.channel.send(`Total Members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}me`) {
+		message.channel.send(`Username: ${message.author.username}`);
+		message.channel.send(`ID: ${message.author.id}`);
 	}
 });
