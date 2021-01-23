da7fb1d7838e688f7bef0d3f226ffdf767d691c4
Thenuka Herath <0033737@jwacs.wa.edu.au>
2021-01-23 14:52:59 +0800

    .....

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client;
-// const config = require('./config.json');
 
 client.on('ready', () => {
 	console.log('READY!');
 });
 
 client.login(process.env.TOKEN);
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		message.channel.send('Pong!');
+	}
+});
